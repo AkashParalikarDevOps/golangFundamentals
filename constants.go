2aafbc9fd08b814f53047b54f8a3ed5689b3e3bd
Akash Paralikar <akashparalikardevops@gmail.com>
2025-07-10 08:23:52 +0000

    area of circle programme

---
+++ constants.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const PI = 3.14
+
+func areaOfCircle(radius float64) float64 {
+	areaOfCircle := PI * radius * radius
+	return areaOfCircle
+}
+
+func main() {
+	var radius float64
+	fmt.Print("Enter the radius of the circle: ")
+	fmt.Scanf("%f", &radius)
+	area := areaOfCircle(radius)
+	fmt.Printf("The area of the circle with radius %.2f is %.2f\n", radius, area)
+}

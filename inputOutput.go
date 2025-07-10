6d6113783e4f318d7af57a4bdc873496fe115d7d
Akash Paralikar <akashparalikardevops@gmail.com>
2025-07-10 07:03:23 +0000

    age detector programme

--- inputOutput.go
+++ inputOutput.go
@@ -3,28 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	daughter := ""
+	var name string
 	age := 0
 	fmt.Print("Enter the child's name:")
-	fmt.Scanf("%s/n", &daughter)
+	fmt.Scanf("%s", &name)
 	fmt.Print("Enter the child's age:")
 	fmt.Scanf("%d", &age)
-	fmt.Printf("%s is %d years old\n", daughter, age)
+	fmt.Printf("%s is %d years old\n", name, age)
 	if age < 18 {
-		fmt.Printf("%s is a minor\n", daughter)
+		fmt.Printf("%s is a minor\n", name)
 	} else {
-		fmt.Printf("%s is an adult\n", daughter)
+		fmt.Printf("%s is an adult\n", name)
 	}
 	if age < 13 {
-		fmt.Printf("%s is a child\n", daughter)
+		fmt.Printf("%s is a child\n", name)
 	}
 	if age >= 13 && age < 18 {
-		fmt.Printf("%s is a teenager\n", daughter)
+		fmt.Printf("%s is a teenager\n", name)
 	}
 	if age >= 18 && age < 65 {
-		fmt.Printf("%s is a young adult\n", daughter)
+		fmt.Printf("%s is a young adult\n", name)
 	}
 	if age >= 65 {
-		fmt.Printf("%s is a senior citizen\n", daughter)
+		fmt.Printf("%s is a senior citizen\n", name)
 	}
 }
